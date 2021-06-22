1be9a3411b7480abc39227bc049cb28b9aff8204
unknown <joemy.flores@hotwork.asia>
2021-06-22 16:52:40 +0800

    test

--- frontend/src/pages/plan/index.jsx
+++ frontend/src/pages/plan/index.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Create from './Create';
+import * as P from 'styled/plan';
 
 export const Index = (props) => {
   return (
     <>
-      <h3 className="text-center">Plans</h3>
+      <h3 className="text-center mb-4 font-weight-bold">Plans</h3>
       <div className="row">
         <div className="col-md-4">
           <Create />
         </div>
 
         <div className="col-md-8">
-          <div className="plan-lists">
-            
-          </div>
+          <P.PlanList className="rounded">
+            <P.Table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Currency</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1097 USD × 2 Monthly</td>
+                  <td>USD</td>
+                  <td>
+                    <button className="btn btn-sm text-primary">Edit</button>
+                    <button className="btn btn-sm text-danger">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </P.Table>
+          </P.PlanList>
         </div>
       </div>
     </>
   );
 }
 
-const mapStateToProps = (state) => ({})
-const mapDispatchToProps = {}
-export default connect(mapStateToProps, mapDispatchToProps)(Index)
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
--- frontend/src/styled/plan.js
+++ frontend/src/styled/plan.js
@@ -4,3 +4,32 @@ export const FormWrapper = styled.div`
   background: #fff;
   padding: 30px;
 `;
+
+export const PlanList = styled.div`
+  background: #fff;
+  padding: 5px 0px;
+`;
+
+export const Table = styled.table`
+  thead {
+    tr {
+      &:first-child {
+        th {
+          border-top: none;
+        }
+      }
+    }
+  }
+  tbody {
+    tr {
+      &:nth-child(odd) td {
+        background: #f9f9f9;
+      }
+      td {
+        color: #6c757d;
+        font-size: 0.9em;
+        font-weight: 500;
+      }
+    }
+  }
+`;
